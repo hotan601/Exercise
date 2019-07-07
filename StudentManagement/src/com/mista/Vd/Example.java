549d3c5f8927dca714fa7db6cb6855df50836456
hotan601 <hovantan601@gmail.com>
2019-07-07 08:50:45 +0700

    123

---
+++ StudentManagement/src/com/mista/Vd/Example.java
@@ -0,0 +1,17 @@
+package com.mista.Vd;
+
+public class Example {
+	public static void main(String[]args) {
+		String str = "Yunlin univerity";
+		System.out.println(str.substring(0,9));//in,cat ra 0 den vt 8;
+		
+		System.out.println(str.indexOf("uni"));//in ra tu 0 den vitri cua no
+		System.out.println(str.lastIndexOf("uni"));//in ra so dem tu vtri 0 den vi tri (n-1).
+	int age = 13;
+	String me = "pS" + age;
+	System.out.println(me);
+	
+	 String myStr = "Hello planet earth, you are a great planet.";
+	    System.out.println(myStr.indexOf("planet"));
+	}
+}
